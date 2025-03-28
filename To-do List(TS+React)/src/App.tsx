6cbdd80ed8800fea96a7d62246d6fe83f83cd123
Seok <namgungseok12@gmail.com>
2025-03-28 16:59:04 +0900

    [week2/mission] TypeScript + React를 활용하여 To-Do List 만들기

--- To-do List(TS+React)/src/App.tsx
+++ To-do List(TS+React)/src/App.tsx
@@ -1,15 +1,18 @@
 
 import './App.css'
+import TodoBefore from './components/TodoBefore'
 import Todo from './components/Todo'
+import { TodoProvider } from './context/TodoContext';
 
 function App() {
- 
 
   return (
     <>
-     <Todo />
+    <TodoProvider>
+      <Todo />
+    </TodoProvider>
     </>
   )
 }
 
-export default App
+export default App;
