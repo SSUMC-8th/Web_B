b2dcef86fefc53f4f14a17a0712d495bccefe33d
Seok <namgungseok12@gmail.com>
2025-03-30 23:21:36 +0900

    [week2/mission] useContext를 활용해서 다크모드 구현하기

---
+++ darkmode/src/06-useContext/ThemeContent.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { THEME, useTheme } from './context/ThemeProvider';
+import { clsx } from 'clsx';
+
+export default function ThemeContent() {
+  const{theme} = useTheme();
+  
+    const isLightMode = theme === THEME.LIGHT;
+  return (
+    <div className={clsx(
+          'p-4 h-dvh w-full',
+          isLightMode ? 'bg-white' : 'bg-gray-800'
+    )}>
+      <h1 
+        className={clsx(
+          'text-wxl font-bold',
+          isLightMode ? 'text-black' : 'text-white'
+        )}
+      >
+        Theme Content
+      </h1>
+    </div>
+  );
+}
---
+++ darkmode/src/App.tsx
@@ -0,0 +1,11 @@
+
+import ContextPage from './06-useContext/ContextPage';
+import './App.css'
+
+export default function App(): React.ReactElement {
+  return (
+    <>
+      <ContextPage />
+    </>
+  );
+}
