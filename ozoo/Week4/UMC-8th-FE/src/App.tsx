dd35afb528b3ccd0c21d790c1f87c1ba5d42ee80
Youngju Lee <oortmealy@gmail.com>
2025-04-10 23:51:47 +0900

    [week4/mission] 로그인 / 회원가입 기능 구현해보기 (feat. 유효성 검사)

---
+++ ozoo/Week4/UMC-8th-FE/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <h1 className='text-2xl text-blue-500'>영주 오즈</h1>;
+  
+}
+
+export default App
