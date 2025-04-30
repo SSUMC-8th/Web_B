4af6638c45573ae99b242c10ed3dee03da2df676
qowldud <bjy6631@daum.net>
2025-05-01 04:38:52 +0900

    [week5/mission]

---
+++ judy/Week5/LP/src/types/LoginType.ts
@@ -0,0 +1,11 @@
+export interface LoginRequest {
+  status: boolean;
+  statusCode: number;
+  message: string;
+  data: {
+    id: number;
+    name: string;
+    accessToken: string;
+    refreshToken: string;
+  };
+}
